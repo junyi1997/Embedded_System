911aad86371554eaca0fc87cf9ef55d285c55955
junyi1997 <q5896799@gmail.com>
2021-04-15 22:31:01 +0800

    'PA1'

--- PA1/src/system.cpp
+++ PA1/src/system.cpp
@@ -298,8 +298,8 @@ System::partitionFirstFit()
     for (int i = 0; i < numThread; i++) {
         std::cout << "aaa  " << aaa << std::endl;
         cpuSet[aaa].pushThreadToCPU(&threadSet[i]);
-        if (cpuSet[aaa].utilization() > 1 && aaa<3) { threadSet[i].setUpCPUAffinityMask(i);aaa++; cpuSet[aaa].pushThreadToCPU(&threadSet[i]); } 
-        if (cpuSet[aaa].utilization() > 1 && aaa ==4) { std::cout << "numThread  " << i << std::endl; }
+        if (cpuSet[aaa].utilization() > 1 && aaa!=3) { threadSet[i].setUpCPUAffinityMask(i);aaa++; cpuSet[aaa].pushThreadToCPU(&threadSet[i]); } 
+        if (cpuSet[aaa].utilization() > 1 && aaa ==3) { std::cout << "numThread  " << i << std::endl; }
         
         cpuSet[aaa].printCPUInformation();
     }
