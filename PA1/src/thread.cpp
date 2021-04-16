b85f1d69face58f27c3b92b9652c1219af38cc32
junyi1997 <q5896799@gmail.com>
2021-04-16 22:25:48 +0800

    'PA1'

--- PA1/src/thread.cpp
+++ PA1/src/thread.cpp
@@ -171,7 +171,7 @@ Thread::matrixMultiplication(void* args)
 
 	/*~~~~~~~~~~~~Your code(PART1)~~~~~~~~~~~*/
     // Set up the affinity mask 
-	obj->setUpCPUAffinityMask(obj->sched_getcpu());
+	obj->setUpCPUAffinityMask(core);
 	/*~~~~~~~~~~~~~~~~~~END~~~~~~~~~~~~~~~~~~*/
 	/* Print Thread information */
 	obj->core = sched_getcpu();
