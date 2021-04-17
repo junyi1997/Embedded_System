b6e05343895ccfeba98706891b83de963a565722
junyi1997 <q5896799@gmail.com>
2021-04-17 13:27:57 +0800

    'PA1'

--- PA1/src/system.cpp
+++ PA1/src/system.cpp
@@ -358,6 +358,21 @@ System::partitionBestFit()
         if (cpuSet[0].utilization() + threadSet[i].utilization() < 1 && cpu_U_0 >= cpu_U_1 && cpu_U_0 >= cpu_U_2 && cpu_U_0 >= cpu_U_3) { threadSet[i].setUpCPUAffinityMask(0); cpuSet[0].pushThreadToCPU(&threadSet[i]); }
         else if (cpuSet[1].utilization() + threadSet[i].utilization() < 1 && cpu_U_1 >= cpu_U_2 && cpu_U_1 >= cpu_U_3) { threadSet[i].setUpCPUAffinityMask(1); cpuSet[1].pushThreadToCPU(&threadSet[i]); }
         else if (cpuSet[2].utilization() + threadSet[i].utilization() < 1 && cpu_U_2 >= cpu_U_3) { threadSet[i].setUpCPUAffinityMask(2); cpuSet[2].pushThreadToCPU(&threadSet[i]); }
+        else if (cpuSet[3].utilization() + threadSet[i].utilization() < 1) { threadSet[i].setUpCPUAffinityMask(3);  cpuSet[3].pushThreadToCPU(&threadSet[i]); }
+        else { std::cout << "Thread-" << i << " is no schedulable" << std::endl; }
+        else { std::cout << "Thread-" << i << " is no schedulable" << std::endl; }
+    }
+    for (int i = 0; i < CORE_NUM; i++) { cpuSet[i].printCPUInformation(); }
+    for (int i = 0; i < CORE_NUM; i++)
+        cpuSet[i].emptyCPU(); // Reset the CPU set
+    for (int i = 0; i < numThread; i++) {
+        float cpu_U_0 = cpuSet[0].utilization();
+        float cpu_U_1 = cpuSet[1].utilization();
+        float cpu_U_2 = cpuSet[2].utilization();
+        float cpu_U_3 = cpuSet[3].utilization();
+        if (cpuSet[0].utilization() + threadSet[i].utilization() < 1 && cpu_U_0 >= cpu_U_1 && cpu_U_0 >= cpu_U_2 && cpu_U_0 >= cpu_U_3) { threadSet[i].setUpCPUAffinityMask(0); cpuSet[0].pushThreadToCPU(&threadSet[i]); }
+        else if (cpuSet[1].utilization() + threadSet[i].utilization() < 1 && cpu_U_1 >= cpu_U_2 && cpu_U_1 >= cpu_U_3) { threadSet[i].setUpCPUAffinityMask(1); cpuSet[1].pushThreadToCPU(&threadSet[i]); }
+        else if (cpuSet[2].utilization() + threadSet[i].utilization() < 1 && cpu_U_2 >= cpu_U_3) { threadSet[i].setUpCPUAffinityMask(2); cpuSet[2].pushThreadToCPU(&threadSet[i]); }
         else if (cpuSet[3].utilization() + threadSet[i].utilization() < 1 ) { threadSet[i].setUpCPUAffinityMask(3);  cpuSet[3].pushThreadToCPU(&threadSet[i]); }
         else { std::cout << "Thread-" << i << " is no schedulable" << std::endl; }
         pthread_create(&threadSet[i].pthreadThread, NULL, threadSet[i].matrixMultiplication, &threadSet[i]);
@@ -399,6 +414,21 @@ System::partitionWorstFit()
     /*~~~~~~~~~~~~Your code(PART2)~~~~~~~~~~~*/
     // Implement parititon first-fit and print result.
     setStartTime();
+    for (int i = 0; i < numThread; i++) {
+        float cpu_U_0 = cpuSet[0].utilization();
+        float cpu_U_1 = cpuSet[1].utilization();
+        float cpu_U_2 = cpuSet[2].utilization();
+        float cpu_U_3 = cpuSet[3].utilization();
+        if (cpuSet[0].utilization() + threadSet[i].utilization() < 1 && cpu_U_0 <= cpu_U_1 && cpu_U_0 <= cpu_U_2 && cpu_U_0 <= cpu_U_3) { threadSet[i].setUpCPUAffinityMask(0); cpuSet[0].pushThreadToCPU(&threadSet[i]); }
+        else if (cpuSet[1].utilization() + threadSet[i].utilization() < 1 && cpu_U_1 <= cpu_U_0 && cpu_U_1 <= cpu_U_2 && cpu_U_1 <= cpu_U_3) { threadSet[i].setUpCPUAffinityMask(1); cpuSet[1].pushThreadToCPU(&threadSet[i]); }
+        else if (cpuSet[2].utilization() + threadSet[i].utilization() < 1 && cpu_U_2 <= cpu_U_1 && cpu_U_2 <= cpu_U_0 && cpu_U_2 <= cpu_U_3) { threadSet[i].setUpCPUAffinityMask(2); cpuSet[2].pushThreadToCPU(&threadSet[i]); }
+        else if (cpuSet[3].utilization() + threadSet[i].utilization() < 1 && cpu_U_3 <= cpu_U_1 && cpu_U_3 <= cpu_U_2 && cpu_U_3 <= cpu_U_0) { threadSet[i].setUpCPUAffinityMask(3);  cpuSet[3].pushThreadToCPU(&threadSet[i]); }
+        else { std::cout << "Thread-" << i << " is no schedulable" << std::endl; }
+        else { std::cout << "Thread-" << i << " is no schedulable" << std::endl; }
+    }
+    for (int i = 0; i < CORE_NUM; i++) { cpuSet[i].printCPUInformation(); }
+    for (int i = 0; i < CORE_NUM; i++)
+        cpuSet[i].emptyCPU(); // Reset the CPU set
     for (int i = 0; i < numThread; i++) {
         float cpu_U_0 = cpuSet[0].utilization();
         float cpu_U_1 = cpuSet[1].utilization();
