1bcff6de9825b4b39518b809addd3ac3e02243f0
junyi1997 <q5896799@gmail.com>
2021-04-17 20:07:01 +0800

    'PA1'

--- PA1/src/thread.cpp
+++ PA1/src/thread.cpp
@@ -204,11 +204,11 @@ Thread::matrixMultiplication(void* args)
 			/*~~~~~~~~~~~~Your code(PART3)~~~~~~~~~~~*/
 			// Obaserve the execute thread on core-0
 		        if (aaa == 0) { std::cout << "Core0 start PID - " << syscall(SYS_gettid) << std::endl; }
-				if (obj->PID != syscall(SYS_gettid))
+				/*if (obj->PID != syscall(SYS_gettid))
 				{
 					std::cout << "Core0 context switch from PID - " << aaa << " to PID - " << syscall(SYS_gettid) << std::endl;
 					aaa = syscall(SYS_gettid);
-				}
+				}*/
 				/*~~~~~~~~~~~~~~~~~~END~~~~~~~~~~~~~~~~~~*/
 			#endif
 	}
