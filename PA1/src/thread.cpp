6e1658d541f41ab1a8d0138a582d85194096a835
junyi1997 <q5896799@gmail.com>
2021-04-17 16:53:30 +0800

    'PA1'

--- PA1/src/thread.cpp
+++ PA1/src/thread.cpp
@@ -251,6 +251,7 @@ Thread::setUpScheduler()
 {
 	/*~~~~~~~~~~~~Your code(PART3)~~~~~~~~~~~*/
     // Set up the scheduler for current thread 為當前線程設置調度程序
+    #include <sched.h>
 	struct sched_param sp;
 	sp.sched_priority = sched_get_priority_max(SCHED_FIFO); 
 	ret = sched_setscheduler(0, SCHED_FIFO, &sp);
