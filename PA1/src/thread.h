dada94dd638cf75eeec27c32af2b2461cbd1712d
junyi1997 <q5896799@gmail.com>
2021-04-19 09:25:10 +0800

    'PA1'

--- PA1/src/thread.h
+++ PA1/src/thread.h
@@ -16,7 +16,6 @@
 #include "config.h"
 #include "../libs/check.h"
 
-#include "cpu.h"
 
 static pthread_mutex_t count_Mutex;	// pthread lock variable
 static int current_PID = -1;
@@ -69,7 +68,7 @@ class Thread
 	float **matrix;              // 2-D matrix
 	float **singleResult;        // Store the single-core matrix multiplication result
 	float **multiResult;		 // Store the multi-core matrix multiplication result
-
+    CPU* cpuSet;               // list of cpu
 
     Check *check;
 };
