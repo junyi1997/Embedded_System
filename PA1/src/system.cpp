2e722f2e18c52c9433a2411cac23497e433f84c1
junyi1997 <q5896799@gmail.com>
2021-04-19 10:40:47 +0800

    'PA1'

--- PA1/src/system.cpp
+++ PA1/src/system.cpp
@@ -363,7 +363,7 @@ System::partitionFirstFit()
         if (aaaa == 1) {
             #if (PART == 3)
                 if (aaa == 0) { std::cout << "Core0 start PID - " << threadSet[i].PID_self() << std::endl; }
-                else { std::cout << "Core0 context switch from PID - " << aaa << " to PID - " << threadSet[i].PID_self() << std::endl; }
+                else { std::cout << "Core0 context switch from PID - " << i << " to PID - " << threadSet[i].PID_self() << std::endl; }
                 aaa = threadSet[i].PID_self();
 
 
