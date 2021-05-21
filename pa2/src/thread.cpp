2f67290647589a2d9c0f0c4ac6c2119b93efb296
junyi1997 <q5896799@gmail.com>
2021-05-21 11:17:31 +0800

    'PA2'

--- pa2/src/thread.cpp
+++ pa2/src/thread.cpp
@@ -177,14 +177,18 @@ Thread::matrixMultiplication(void* args)
 
 	// Multiplication for MULTI_TIME times
     for (int num_multi = 0; num_multi < MULTI_TIME; num_multi++) {
-
+#if (PART != 1)
+		std::cout << "main() is ready.\n" << std::endl;
+		pthread_barrier_wait(obj->barr);
+		std::cout << "main() is going!\n" << std::endl;
+#endif
 	    for (int i = obj->startCalculatePoint; i < obj->endCalculatePoint; i++) {
 
 	    	for (int j = 0 ; j < obj->matrixSize; j++) {
 
 #if (PART != 2)
 				//std::cout << "main() is ready.\n" << std::endl;
-				pthread_barrier_wait(obj->barr);
+				//pthread_barrier_wait(obj->barr);
 				//std::cout << "main() is going!\n" << std::endl;
 				/*~~~~~~~~~~~~Your code(PART1)~~~~~~~~~~~*/
 				pthread_mutex_lock(obj->ioMutex);
