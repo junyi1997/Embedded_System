0f462c162829534e6e2cf310553538637ef39ca6
junyi1997 <q5896799@gmail.com>
2021-05-21 10:36:07 +0800

    'PA2'

--- pa2/src/thread.cpp
+++ pa2/src/thread.cpp
@@ -185,13 +185,10 @@ Thread::matrixMultiplication(void* args)
 
 #if (PART != 2)
 				//std::cout << "main() is ready.\n" << std::endl;
-				//pthread_barrier_wait(&barr);
+				pthread_barrier_wait(obj->barr);
 				//std::cout << "main() is going!\n" << std::endl;
 				/*~~~~~~~~~~~~Your code(PART1)~~~~~~~~~~~*/
 				pthread_mutex_lock(obj->ioMutex);
-				std::cout << "Synchronize method not supported." << std::endl;
-				pthread_mutex_unlock(obj->ioMutex);
-				//pthread_mutex_lock(&count_mutex);
 				/*~~~~~~~~~~~~~~~~~~END~~~~~~~~~~~~~~~~~~*/
                 *obj->sharedSum = 0;
 	    		for (int k = 0 ; k < obj->matrixSize; k++)
@@ -199,7 +196,7 @@ Thread::matrixMultiplication(void* args)
 
                 obj->multiResult [i][j] = *obj->sharedSum;
 				/*~~~~~~~~~~~~Your code(PART1)~~~~~~~~~~~*/
-				//pthread_mutex_unlock(&count_mutex);
+				pthread_mutex_unlock(obj->ioMutex);
 				/*~~~~~~~~~~~~~~~~~~END~~~~~~~~~~~~~~~~~~*/
 #else
 
