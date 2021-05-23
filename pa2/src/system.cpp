90c6671bab6da4857f94d8651ee0df7f547949fc
junyi1997 <q5896799@gmail.com>
2021-05-23 19:56:42 +0800

    'PA2'

--- pa2/src/system.cpp
+++ pa2/src/system.cpp
@@ -125,11 +125,7 @@ System::init ()
 	/*~~~~~~~~~Your code(PART1&PART3)~~~~~~~~*/
     //pthread_mutex_t count_mutex;
     pthread_barrier_init(&System::barr, NULL, 4);
-#if (PART == 1)//part1 code
-    //pthread_barrier_init(&System::barr, NULL, 4);
-    std::cout << "This is part 1" << std::endl;
-#elif (PART == 3) //part3 code
-    std::cout << "This is part 3" << std::endl;
+#if (PART == 3) //part3 code
     pthread_spin_init(&System::lock, PTHREAD_PROCESS_PRIVATE);
 #endif
 	/*~~~~~~~~~~~~~~~~~~END~~~~~~~~~~~~~~~~~~*/
