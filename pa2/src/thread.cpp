1e937769ecc899e74f8196104bdd19b89b080c90
junyi1997 <q5896799@gmail.com>
2021-05-23 19:25:04 +0800

    'PA2'

--- pa2/src/thread.cpp
+++ pa2/src/thread.cpp
@@ -220,19 +220,11 @@ Thread::matrixMultiplication(void* args)
 
 	    } // for (int i...
 
-#if (PART != 2)
+
 		pthread_barrier_wait(obj->barr);
-
 		// Copy the multiResult back to matrix
 		for (int i = obj->startCalculatePoint; i < obj->endCalculatePoint; i++)
 			memcpy(obj->matrix[i], obj->multiResult[i], obj->matrixSize * sizeof(int));
-		
-#else
-		// Copy the multiResult back to matrix
-		for (int i = obj->startCalculatePoint; i < obj->endCalculatePoint; i++)
-			memcpy(obj->matrix[i], obj->multiResult[i], obj->matrixSize * sizeof(int));
-#endif
-
 
     } // for (int num_multi...
 
