dbdd466ecb5234121f58d45d3882d8bc9d8f7304
junyi1997 <q5896799@gmail.com>
2021-05-23 19:08:41 +0800

    'PA2'

--- pa2/src/thread.cpp
+++ pa2/src/thread.cpp
@@ -209,7 +209,7 @@ Thread::matrixMultiplication(void* args)
 #else
 
 	        /*~~~~~~~~~~~~Your code(PART2)~~~~~~~~~~~*/
-			*obj->sharedSum = 0;
+			*obj->multiResult = 0;
 			for (int k = 0; k < obj->matrixSize; k++)
 				*obj->multiResult += obj->matrix[i][k] * obj->matrix[k][j];
 			obj->multiResult[i][j] = *obj->multiResult;
